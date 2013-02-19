8d4516f54099be128c6ec8d202b2b3b33fa2e9f9
Richard Janík <littlejohn@mail.muni.cz>
2013-02-19 23:00:24 +0100

    A pre-meeting commit. This is a rough version, there have been major changes to client app (and wrestling with maven) and some reorganization in the beans.

---
+++ jbebt-client/src/main/java/org/jboss/test/jbebt/client/Variables.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jboss.test.jbebt.client;
+
+import org.jboss.test.jbebt.ejb.RemoteSFSBImpl;
+
+/**
+ *
+ * @author rjanik
+ */
+public class Variables {
+	
+	public static final String moduleName = "jbebt-ejb-1.0.0-SNAPSHOT";
+	
+}
---
+++ jbebt-ejb/src/main/java/org/jboss/test/jbebt/ejb/RemoteSLSB.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jboss.test.jbebt.ejb;
+
+import javax.ejb.Remote;
+
+/**
+ * A simple interface for remote stateless session beans.
+ *
+ * @author rjanik
+ */
+@Remote
+public interface RemoteSLSB {
+	
+	/**
+	 * A simple method that returns a simple message. Node independent.
+	 *
+	 * @return "Hello world!" message
+	 */
+	public String getMessage();
+
+	/**
+	 * A simple method that returns a node name. Node dependent.
+	 *
+	 * @return Name of the node the ejb is currently deployed on.
+	 */
+	public String getNodeName();
+	
+}
